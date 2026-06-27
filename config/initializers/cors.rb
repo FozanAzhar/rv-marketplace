2afdf87f8beb90695bfc042fc691dbe189ef19f0
FozanAzhar <fozanazhar28@gmail.com>
2026-06-28 02:22:30 +1000

    Add front-end listing page

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,16 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "localhost:5173", "127.0.0.1:3000", "127.0.0.1:5173"
+    origins(
+      "localhost:3000",
+      "localhost:5173",
+      "localhost:5500",
+      "localhost:8080",
+      "127.0.0.1:3000",
+      "127.0.0.1:5173",
+      "127.0.0.1:5500",
+      "127.0.0.1:8080"
+    )
 
     resource "*",
       headers: :any,
